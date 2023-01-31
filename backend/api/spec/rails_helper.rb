e5f5d2d811dc44f4bf27bfc717916a882cbe9f5e
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-01-31 15:56:54 +0900

    update:rspecの設定を追加。

--- backend/api/spec/rails_helper.rb
+++ backend/api/spec/rails_helper.rb
@@ -1,11 +1,16 @@
+# RspecのRails特有の設定を記述するファイル
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+# [このファイルは、'rails generate rspec:install' を実行すると spec/ にコピーされます。]
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
+# [環境がproductionの場合、データベースの切り詰めを防止する]
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+# [この行の下に追加のrequireを追加します。この時点までRailsはロードされていません!]
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -14,6 +19,12 @@ require 'rspec/rails'
 # run twice. It is recommended that you do not name files matching this glob to
 # end with _spec.rb. You can configure this pattern with the --pattern
 # option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+# [spec/support/ およびそのサブディレクトリにある、カスタムマッチャーやマクロなどを含むサポート用
+# Rubyファイルが必要です。spec/**/*_spec.rb` にマッチするファイルは、デフォルトで spec ファイル
+# として実行されます。つまり、spec/supportにある_spec.rbで終わるファイルは、specとして要求され、
+# 実行されるので、specが2度実行されることになります。このグロブにマッチするファイル名を_spec.rbで
+# 終わらないようにすることをお勧めします。このパターンはコマンドラインの--patternオプション、また
+# は~/.rspec、.rspec、`.rspec-local`で設定することができます。]
 #
 # The following line is provided for convenience purposes. It has the downside
 # of increasing the boot-up time by auto-requiring all files in the support
@@ -60,4 +71,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # 1
+  config.include FactoryBot::Syntax::Methods
 end
+
+=begin
+=        ==        ==        ==        ==        ==        ==        ==        ==        =
+1
+#このコードを加えることで、以下のようにrspecのテストコード中でFactory_botのメソッドを使用する際に、クラス
+名の指定を省略できるようになる。
+#通常FactoryBotをつけないと、メソッドを呼べない
+user = FactoryBot.create(:user)
+
+#設定を追加することで、FactoryBotの記述が省略できる。
+user = create(:user)
+
+=end

a7dc854d538bb24777e2b6ce631858a66825b335
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-04-02 13:16:41 +0900

    add:create-sessions-controller

--- backend/api/app/controllers/api/v1/auth/registrations_controller.rb
+++ backend/api/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,8 +1,36 @@
+# 1
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   private
 
+  2
   def sign_up_params
     # サインアップ時に登録できるカラムを指定
     params.permit(:email, :password, :password_confirmation, :name)
   end
 end
+
+=begin
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+このコードは、DeviseTokenAuthのRegistrationsControllerを継承した
+Api::V1::Auth::RegistrationsControllerクラスで、サインアップ時に許可されるパラメータを指定する
+sign_up_paramsメソッドを定義しています。params.permitで許可するカラムを指定しています。
+このコードの利用意図は、サインアップ時に必要なカラムを指定することで、不必要なパラメータが登録されることを防止し、セ
+キュリティを強化することです。
+サインアップ時に必要なカラムを指定することは一般的であり、Railsのstrong parametersを使った設定も同様です。また
+、Deviseの設定においても、ユーザー登録に必要なカラムを指定する設定が一般的です。
+
+================================================================================================
+2
+sign_up_paramsメソッドは、サインアップ時に登録できるカラムを指定するために使用されます。具体的には、paramsオブ
+ジェクトからemail、password、password_confirmation、nameの4つのパラメータのみを取得します。
+sign_up_paramsメソッドの目的は、セキュリティ上の理由から、不要な情報が含まれている場合に備えて、許可されたパラメ
+ータ以外は受け付けないようにすることです。
+これはDeviseやDeviseTokenAuthなどの認証Gemを使用する場合によく見られます。また、Strong Parametersを使用する
+Railsの標準的な方法でもあります。
+sign_up_params は、Devise Token Auth の RegistrationsController に実装されているアクションです。ただし、
+Devise の RegistrationsController でも、同様のアクションが存在するため、混乱が生じる場合があります。
+Devise Token Auth の sign_up_params は、新規ユーザー登録に必要なパラメータを許可するストロングパラメーターを
+定義するために使用されます。
+
+=end
--- backend/api/app/controllers/application_controller.rb
+++ backend/api/app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
 class ApplicationController < ActionController::API
         include DeviseTokenAuth::Concerns::SetUserByToken
 end
+
+=begin
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+DeviseTokenAuth::Concerns::SetUserByToken
+
+devise_token_auth が提供するモジュールの一つで、APIでトークンベースの認証を行う際に、トークンに基づいて、様々
+なメソッドが使えるようになります。
+
+set_user_by_token ：渡されたトークンに対応するユーザーを設定する
+current_user      ：現在ログインしているユーザーを返す
+authenticate_user!：ユーザーがログインしているかどうかをチェックする
+user_signed_in?   ：ユーザーがログインしているかどうかを返す
+
+これらのメソッドを利用することで、トークンに基づくユーザーの認証や、ログインしているユーザーの取得などが簡単に行え
+るようになります。
+
+------------------------------------------------------------------------------------------------
+Concernsは、関連するメソッドや機能をモジュール方式でグループ化し、複数のクラスやモジュールに含めることができる方
+法です。Ruby on Railsアプリケーションでコードの繰り返しを避け、再利用性を促進する方法です。
+
+=end

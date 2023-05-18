3682b2b78c58a71538e1365eb7814333da155fee
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-05-18 13:17:50 +0900

    refactor:ユーザー一覧ページのページネーションの実装をカスタムフックに切り出し

---
+++ frontend/front/src/hooks/useUsersPagination.ts
@@ -0,0 +1,161 @@
+import { useRouter } from 'next/router';
+import { useCallback, useEffect, useState } from 'react';
+import { userIndex } from '../api/user';
+import { useAlertContext } from '../contexts/AlertContext';
+import { User } from '../types';
+
+// ユーザー一覧ページネーション用のカスタムフック
+export const useUsersPagination = (itemsPerPage: number) => {
+  // 1 user一覧の状態を管理するステート。User型の配列を保持する。
+  const [users, setUsers] = useState<User[]>([]);
+  // 総ユーザー数を保持するステート
+  const [totalUsers, setTotalUsers] = useState(0);
+  // 現在のページ番号を保持するステート
+  const [currentPage, setCurrentPage] = useState(0);
+  // AlertContextから、setAlertMessage, setAlertOpen, setAlertSeverityを受け取る。
+  const { setAlertMessage, setAlertOpen, setAlertSeverity } = useAlertContext();
+  const router = useRouter();
+  // ------------------------------------------------------------------------------------------------
+  // 3 ユーザー一覧を取得して、ステートに格納し、ページネーションを表示し、ページをクリックした時の処理を定義
+  const handleGetUsers = useCallback(
+    async (page: number) => {
+      try {
+        // userIndex関数を使用して、ユーザー一覧を取得する
+        const res = await userIndex(page, itemsPerPage);
+        // ユーザー一覧をステートに格納する
+        setUsers(res.data.users);
+        // 総ユーザー数をステートに格納する
+        setTotalUsers(res.data.totalUsers);
+      } catch (err: any) {
+        setAlertSeverity('error');
+        setAlertMessage(`${err.response.data.errors[0]}`);
+        setAlertOpen(true);
+        setTimeout(() => {
+          router.push('/');
+        }, 2000);
+      }
+    },
+    [itemsPerPage, router, setAlertMessage, setAlertOpen, setAlertSeverity]
+  );
+
+  // 4 ページネーションのページをクリックした時に実行される関数
+  useEffect(() => {
+    handleGetUsers(currentPage);
+  }, [currentPage, handleGetUsers]);
+
+  // 5
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    setCurrentPage(selectedItem.selected);
+  };
+
+  return { users, totalUsers, handlePageChange };
+};
+
+/*
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+useState<User[]>() を使用して、users の型を User[] に設定します。これにより、users にはユーザー情報の配列が
+格納されます。
+
+================================================================================================
+3
+useCallbackは、Reactのフックの一つであり、コンポーネント内で定義された関数をキャッシュするためのメモ化関数です。
+useCallbackを使用すると、同じ関数インスタンスを再生成せず、再利用することができ、パフォーマンスを改善することがで
+きます。
+第1引数には、メモ化する関数の定義を記述。第2引数には、依存関係を配列で指定する。
+ReactのコンポーネントでReactPaginateを使用して非同期のAPI呼び出しを行う場合、useEffectとuseCallbackを使うこ
+とが一般的です。
+useEffectの依存関係に関数を入れると、再評価されるたびにuseEffectが実行され、無限ループが発生するためです。
+
+------------------------------------------------------------------------------------------------
+[itemsPerPage, router, setAlertMessage, setAlertOpen, setAlertSeverity]
+useCallbackは第二引数の依存配列の値が変化した時にのみコールバック関数を再生成します。
+ここではitemsPerPageとrouterが依存配列に指定されており、これらの値が変わった時にのみhandleGetUsers関数が再生
+成されます。
+itemsPerPageはユーザー一覧取得の際に1ページあたりの表示件数として使用されます。この値が変化すると取得するユーザー
+一覧が変わるため、handleGetUsers関数を再生成する必要があります。
+routerはエラー発生時にルートへリダイレクトするために使用されます。通常、router自体の値が変わることはありませんが、
+依存配列に含めることでrouterの参照が常に最新のものであることを保証します。
+------------------------------------------------------------------------------------------------
+（setAlertMessage, setAlertOpen,setAlertSeverity）を指定する理由
+useCallbackフックは、特定の関数をメモ化するためのフックです。メモ化とは、同じ引数で関数が呼ばれた場合に、前回の結
+果を再利用することです。
+useCallbackは、依存配列（第二引数）に指定された変数が変化した時のみ新しい関数を生成します。それ以外の時は、以前の
+関数を再利用します。
+依存配列に指定されていない変数がその関数内で使われると、その変数が変更されたときにも関数が新しく生成されず、古い値を
+参照し続けてしまいます。そのため、関数内で使用している変数は依存配列に含める必要があります。
+useCallbackフックは、その依存配列にリストされた値が変更されたときだけ再計算される関数をメモ化します。
+を使用しているために発生しています。依存配列に含まれていない変数を使用すると、その変数の値が変更されても、
+useCallbackでメモ化された関数は更新されません。
+useAlertContextというカスタムフックから得られているこれらの関数（setAlertMessage,
+setAlertOpen,setAlertSeverity）は、状態変更関数です。つまり、これらはReactの状態を変更するための関数です。
+useCallback内でこれらの関数を利用している場合、それらは依存配列に含めるべきです。なぜなら、これらの関数が変わると、
+useCallbackで作られた関数の動作が変わる可能性があるからです。
+
+================================================================================================
+4
+[currentPage, handleGetUsers]
+依存配列にcurrentPageとhandleGetUsersを指定する理由
+
+currentPage
+ページ番号が変更された時に、handleGetUsers関数を実行するためです。currentPageが変更されると、ページ番号に基づい
+て異なるユーザー一覧を取得する必要があります。currentPageの値が変わるたびに、handleGetUsers関数が実行され、新し
+いページ番号に基づいたユーザー一覧が取得されます。
+currentPageが変化すると、新しいページのユーザー一覧を取得する必要があります。そのため、currentPageが変化した時に
+handleGetUsersを実行します。
+
+handleGetUsers
+handleGetUsers関数自体も依存配列に含まれています。これは、handleGetUsers関数でで使用されている変数のpage,
+itemsPerPageが変更される可能性があるためです。もし変更された場合、関数は変わりませんが、変数や値が変更されると、
+依存配列が変更されたとみなされ。useEffectフックは再度実行されます。
+この仕組みにより、最新のhandleGetUsers関数が適用され、期待通りの挙動を実現することができます。
+handleGetUsers関数で参照している変数や値が変更されると、依存配列が変更されたとみなされます。その結果、useEffect
+フックは再実行され、最新の値や状態を反映した処理が実行されます。
+handleGetUsersが再生成されると（useCallbackの依存配列の値[itemsPerPage]が変わった場合）、その新しいバージョン
+を使用してユーザー一覧を取得する必要があります。そのため、handleGetUsersが変化した時にも副作用関数を実行します。
+handleGetUsersが依存配列に含まれていないと、handleGetUsersが再生成された時に副作用関数が実行されないため、最新の
+handleGetUsersを使用してユーザー一覧を取得することができません。handleGetUsersが再生成されるたびに、useEffect
+内のコードが再実行され、最新のhandleGetUsersを使用してユーザーデータを取得し、ページを更新することが保証されます。
+
+================================================================================================
+5
+handlePageChange
+ページネーションのページがクリックされた時に実行され、クリックされたページ番号を受け取り、
+setCurrentPage関数を使用して現在のページ番号を更新します。
+
+handlePageChange関数が発火すると、setCurrentPageが呼び出されてcurrentPageのステートが更新されます。
+このcurrentPageの更新により、useEffectの依存配列にcurrentPageが含まれているため、useEffectが発火します。これに
+よりhandleGetUsers関数が呼び出され、新しいページのユーザーデータが取得されます。
+一方、useCallbackは依存配列にitemsPerPageとrouterが含まれているため、これらの値が変化しない限り発火しません。
+handlePageChange関数はこれらの値を変更しないので、useCallbackは発火しません。
+ただし、useCallbackによってメモ化されたhandleGetUsers関数がuseEffectによって呼び出されるため、関数自体は実行さ
+れます。
+
+@          @@          @@          @@          @@          @@          @@          @@          @
+================================================================================================
+初回レンダリング時
+Indexコンポーネントがレンダリングされると、初期化時のステート設定が行われます。ここでcurrentPageは0に設定されます。
+useStateフックによって、users, totalUsers, currentPageの初期値が設定される。
+次に、useCallbackフックが発火し、handleGetUsers関数が定義されます。この時、依存配列にitemsPerPageとrouterが指
+定されていますが、これらの値は変化しないので、この関数は基本的に再生成されません。
+useEffectフックが発火します。依存配列にcurrentPageとhandleGetUsersが指定されているため、これらの値の変化により
+今後handleGetUsersが再実行されます。
+handleGetUsers関数は、userIndex関数を使用して、指定されたページのユーザー一覧を取得する。
+初回レンダリング時、currentPageは0なので、handleGetUsersが実行され、ページ0（最初のページ）のユーザーデータを取
+得します。
+------------------------------------------------------------------------------------------------
+handlePageChange着火時
+ページネーションのページがクリックされると、handlePageChangeが発火します。
+handlePageChangeが呼び出されると、setCurrentPageが実行されてcurrentPageのステートが更新され、選択されたページ
+の番号が保存されます。これによりページが更新されます。
+currentPageが更新されると、useEffectフックが発火します。これはcurrentPageがuseEffectの依存配列に含まれているた
+めです。
+useEffect内で、新しく設定されたcurrentPage値を元にhandleGetUsersが実行され、新しいページのユーザーデータを取得
+します。
+handleGetUsersにより、userIndex関数を使用して、取得したユーザー一覧と総ユーザー数をuseStateフックを使ってusers
+とtotalUsersに格納する。
+一方、useCallbackは依存配列にitemsPerPageとrouterが含まれているため、これらの値が変化しない限り発火しません。
+handlePageChange関数はこれらの値を変更しないので、useCallbackは発火しません。
+ただし、useCallbackによってメモ化されたhandleGetUsers関数がuseEffectによって呼び出されるため、関数自体は実行さ
+れます。
+*/
--- frontend/front/src/pages/users/index.tsx
+++ frontend/front/src/pages/users/index.tsx
@@ -1,61 +1,18 @@
-import { useRouter } from 'next/router';
-import { useCallback, useEffect, useState } from 'react';
 import ReactPaginate from 'react-paginate';
-import { userIndex } from '../../api/user';
 import UserListItem from '../../components/atoms/UserListItem';
 import Layout from '../../components/layout/Layout';
-import { User } from '../../types';
+import { useUsersPagination } from '../../hooks/useUsersPagination';
 
 const Index = () => {
-  // 1 user一覧の状態を管理するステート
-  const [users, setUsers] = useState<User[]>([]);
-  const [alertOpen, setAlertOpen] = useState(false);
-  // 総ユーザー数を保持するステート
-  const [totalUsers, setTotalUsers] = useState(0);
-  const [alertSeverity, setAlertSeverity] = useState<'error' | 'success'>('error');
-  const [alertMessage, setAlertMessage] = useState('');
-  const [currentPage, setCurrentPage] = useState(0);
-  // const { setAlertMessage, setAlertOpen, setAlertSeverity } = useAlertContext();
+  // 1ぺージに表示するユーザー数
   const itemsPerPage = 10;
-  const router = useRouter();
-  // ------------------------------------------------------------------------------------------------
-  // 3 ユーザー一覧を取得する関数
-  const handleGetUsers = useCallback(
-    async (page: number) => {
-      try {
-        // userIndex関数を使用して、ユーザー一覧を取得する
-        const res = await userIndex(page, itemsPerPage);
-        console.log(`handleGetUsersのres.data.users:${JSON.stringify(res.data.users)}`);
-        console.log(`handleGetUsersのres.data:${JSON.stringify(res.data)}`);
-        // ユーザー一覧をステートに格納する
-        setUsers(res.data.users);
-        setTotalUsers(res.data.totalUsers);
-      } catch (err: any) {
-        setAlertSeverity('error');
-        setAlertMessage(`${err.response.data.errors[0]}`);
-        setAlertOpen(true);
-        setTimeout(() => {
-          router.push('/');
-        }, 2000);
-      }
-    },
-    [itemsPerPage, router]
-  );
+  // 1 ユーザー一覧を取得して、ステートに格納し、ページをクリックした時の処理を定義したカスタムフック
+  const { users, totalUsers, handlePageChange } = useUsersPagination(itemsPerPage);
 
-  // 4 ページネーションのページをクリックした時に実行される関数
-  useEffect(() => {
-    handleGetUsers(currentPage);
-  }, [currentPage, handleGetUsers]);
-
-  const handlePageChange = (selectedItem: { selected: number }) => {
-    setCurrentPage(selectedItem.selected);
-  };
-  // ================================================================================================
   return (
     <Layout title={'Users'}>
       <div className='flex flex-1 flex-col'>
         <div className='flex flex-1 flex-col'>
-          {/* h1タグに挟まれた文字列は、h1タグの子要素 */}
           <h1 className='mx-auto mb-2 flex h-10 items-center justify-center text-lg font-semibold sm:text-2xl md:h-14 md:text-4xl'>
             All Users
           </h1>
@@ -69,9 +26,9 @@ const Index = () => {
         <ReactPaginate
           previousLabel={'<'}
           nextLabel={'>'}
-          pageCount={Math.ceil(totalUsers / itemsPerPage)} // 総ユーザー数とアイテム数からページ数を計算
-          marginPagesDisplayed={1} //先頭(前)と末尾(後)に表示する何ページ目の数。今回は2としたので、前1,2,…,7,8後のように表示されます。
-          pageRangeDisplayed={1} //上記の「今いるページの前後」の番号をいくつ表示させるかを決めます。
+          pageCount={Math.ceil(totalUsers / itemsPerPage)}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={1}
           onPageChange={handlePageChange}
           containerClassName={'pagination justify-around flex space-x-3 h-12 md:text-2xl text-xl'}
           activeClassName={'active bg-basic-pink'}
@@ -82,12 +39,12 @@ const Index = () => {
 };
 
 export default Index;
-
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @
 1
-useState<User[]>() を使用して、users の型を User[] に設定します。これにより、users にはユーザー情報の配列が
-格納されます。
+usersは、ユーザーの配列。
+totalUsersは、ユーザーの総数。
+handlePageChangeは、ページネーションのページ変更時に実行する関数。
 
 ================================================================================================
 2
@@ -130,55 +87,6 @@ containerClassName:
 activeClassName:
 現在のページに付与されるクラス名を表す文字列。CSSでこのクラス名を指定することで、現在のページのスタイリングを行えま
 す。
-================================================================================================
-3
-useCallbackは、Reactのフックの一つであり、コンポーネント内で定義された関数をキャッシュするためのメモ化関数です。
-useCallbackを使用すると、同じ関数インスタンスを再生成せず、再利用することができ、パフォーマンスを改善することがで
-きます。
-第1引数には、メモ化する関数の定義を記述。第2引数には、依存関係を配列で指定する。
-ReactのコンポーネントでReactPaginateを使用して非同期のAPI呼び出しを行う場合、useEffectとuseCallbackを使うこ
-とが一般的です。
-useEffectの依存関係に関数を入れると、再評価されるたびにuseEffectが実行され、無限ループが発生するためです。
-
-------------------------------------------------------------------------------------------------
-[itemsPerPage, router]
-依存配列にitemsPerPageとrouterを指定する理由は、これらの値が変更された場合にhandleGetUsers関数を再計算するた
-めです。handleGetUsers関数内でitemsPerPageとrouterを使用しているため、これらの値が変更された時に新しい関数を生
-成するように指定しています。これにより、不要な関数の再生成を防ぎ、パフォーマンスを向上させることができます。
 
 ================================================================================================
-4
-[currentPage, handleGetUsers]
-依存配列にcurrentPageとhandleGetUsersを指定する理由は、これらの値が変更された場合にuseEffect内の関数を実行す
-るためです。currentPageが変更されると新しいページのユーザー情報を取得し、handleGetUsersが変更されると新しい関数
-でユーザー情報を取得するためにこれらを依存配列に含めています。
-
-@          @@          @@          @@          @@          @@          @@          @@          @
-初回レンダリング時の関数の発火順番ときっかけ:
-
-1. useStateフックによって、users, alertOpen, totalUsers, alertSeverity, alertMessage, currentPageの
-初期値が設定される。
-
-2. ページがレンダリングされ、ReactPaginateコンポーネントが表示される。
-
-3. handleGetUsers関数が初回レンダリング時に、useEffectフックによって呼び出される。
-- currentPageとhandleGetUsersを依存関係として渡しているため、currentPageが更新されるたびにhandleGetUsersが
-呼び出される。
-- handleGetUsers関数は、userIndex関数を使用して、指定されたページのユーザー一覧を取得する。
-- 取得したユーザー一覧と総ユーザー数をuseStateフックを使ってusersとtotalUsersに格納する。
-
-4. handlePageChange関数が、ReactPaginateコンポーネントによって呼び出される。
-- 選択されたページの番号がsetCurrentPageによって、currentPageステートに保存される。
-
-================================================================================================
-onPageChangeが発火した際の関数の発火順番ときっかけ:
-
-1. handlePageChange関数が、ReactPaginateコンポーネントによって呼び出される。
-- 選択されたページの番号がsetCurrentPageによって、currentPageステートに保存される。
-
-2. useEffectフックによって、handleGetUsers関数が呼び出される。
-- currentPageとhandleGetUsersを依存関係として渡しているため、currentPageが更新されるたびにhandleGetUsersが
-呼び出される。
-- handleGetUsers関数は、userIndex関数を使用して、指定されたページのユーザー一覧を取得する。
-- 取得したユーザー一覧と総ユーザー数をuseStateフックを使ってusersとtotalUsersに格納する。
 */

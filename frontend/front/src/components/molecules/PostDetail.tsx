c4f0b65b4f245991ca2d11d860439fe6ea717c01
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-04 17:45:56 +0900

    add:PostDetailを仮実装

--- frontend/front/src/components/molecules/PostDetail.tsx
+++ frontend/front/src/components/molecules/PostDetail.tsx
@@ -1,3 +1,28 @@
+import { useRouter } from 'next/router';
+import { usePostContext } from '../../contexts/PostContext';
+
 export const PostDetail = () => {
-  return <div>PostDetail</div>;
+  const { posts } = usePostContext();
+  const router = useRouter();
+  const { id } = router.query;
+  console.log(`ポスト：${JSON.stringify(posts)}`);
+  const post = posts?.find((post) => post.id === Number(id));
+  return (
+    <div className='flex flex-1 flex-col'>
+      <h1 className=' flex h-20 items-center justify-center pt-4 text-2xl font-semibold md:text-4xl'>投稿詳細ページ</h1>
+      <div>
+        {post ? (
+          <div>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            {/* 他の投稿詳細情報をここに表示 */}
+          </div>
+        ) : (
+          <p>投稿が見つかりませんでした。</p>
+        )}
+      </div>
+      <div className='flex flex-1 flex-col'></div>
+      <div></div>
+    </div>
+  );
 };
--- frontend/front/src/pages/post/[id]/index.tsx
+++ frontend/front/src/pages/post/[id]/index.tsx
@@ -1,18 +1,9 @@
 // frontend/front/src/pages/posts/[id]/index.tsx
 
-import { useRouter } from 'next/router';
-import React from 'react';
+import { PostDetail } from '../../../components/molecules/PostDetail';
 
-const PostDetail: React.FC = () => {
-  const router = useRouter();
-  const { id } = router.query;
-
-  return (
-    <div>
-      <h1>Post Detail - {id}</h1>
-      {/* ここに各postの詳細情報を表示します */}
-    </div>
-  );
+const PostDetailPage = () => {
+  return <PostDetail></PostDetail>;
 };
 
-export default PostDetail;
+export default PostDetailPage;

78b73bbf078890948cc6772bba5e9917645347b8
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-04 15:38:42 +0900

    update:post新規作成ページに、useRedirectIfNotAuthorizedを追加

--- frontend/front/src/pages/post/new.tsx
+++ frontend/front/src/pages/post/new.tsx
@@ -2,8 +2,10 @@
 
 import Layout from '../../components/layout/Layout';
 import { PostFormPage } from '../../components/templates/PostFormPage';
+import { useRedirectIfNotAuthorized } from '../../hooks/useRedirectIfNotAuthorized';
 
 const NewPost = () => {
+  useRedirectIfNotAuthorized();
   return (
     <Layout title='Post Form'>
       <PostFormPage></PostFormPage>

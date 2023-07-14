d441285f4830ce7dd07fc8ab66b6a0708bc82cf4
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-14 19:05:43 +0900

    add:getUserFollowersByUserIdを作成

--- frontend/front/src/api/relationship.ts
+++ frontend/front/src/api/relationship.ts
@@ -1,8 +1,20 @@
 import Cookies from 'js-cookie';
 import client from './client';
 
+// ユーザーのフォロー情報を取得するAPI
 export const getUserFollowingByUserId = (userId: number) => {
   return client.get(`/users/${userId}/following`, {
+    headers: {
+      'access-token': Cookies.get('_access_token'),
+      client: Cookies.get('_client'),
+      uid: Cookies.get('_uid'),
+    },
+  });
+};
+
+// ユーザーのフォロワー情報を取得するAPI
+export const getUserFollowersByUserId = (userId: number) => {
+  return client.get(`/users/${userId}/followers`, {
     headers: {
       'access-token': Cookies.get('_access_token'),
       client: Cookies.get('_client'),

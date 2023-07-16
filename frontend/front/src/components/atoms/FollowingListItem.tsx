7ed43c193e45f1232feab064c0b1638b06b9238c
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-16 14:37:18 +0900

    add:FollowingListItemを仮作成

---
+++ frontend/front/src/components/atoms/FollowingListItem.tsx
@@ -0,0 +1,17 @@
+export const FollowingListItem = () => {
+  return <div>FollowingListItem</div>;
+};
+
+/*
+ここFollowingPageの
+[{"id":3,
+"provider":"email",
+"uid":"soso@soso.com",
+"allowPasswordChange":false,
+"name":"soso",
+"email":"soso@soso.com",
+"createdAt":"2023-07-15T03:16:00.160Z",
+"updatedAt":"2023-07-15T03:16:00.160Z",
+"admin":false,
+"profile":"撃つ日欧掛けるしょうゆおとといあおい半額救急車さいぼうひかくするがいよう先ずほひんきゃく出版窓平安むこう長唄おどろく。"},
+*/

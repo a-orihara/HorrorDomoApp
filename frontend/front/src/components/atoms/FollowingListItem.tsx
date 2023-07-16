2c8338e759b100006c2d8228bc2e5d62c22f8db9
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-16 15:05:10 +0900

    add:FollowingListItemにuseGetUserByIdを仮実装

--- frontend/front/src/components/atoms/FollowingListItem.tsx
+++ frontend/front/src/components/atoms/FollowingListItem.tsx
@@ -1,5 +1,37 @@
-export const FollowingListItem = () => {
-  return <div>FollowingListItem</div>;
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import useGetUserById from '../../hooks/user/useGetUserById';
+import { Following } from '../../types/relationship';
+
+type FollowingListItemProps = {
+  following: Following;
+};
+
+// followingはフォローしているユーザーの配列
+export const FollowingListItem = ({ following }: FollowingListItemProps) => {
+  // 選択したidに紐付くuserとpostsを取得
+
+  const router = useRouter();
+  // 1
+  const { id } = router.query;
+  const { user, handleGetUserById } = useGetUserById(id);
+  return (
+    <div>
+      <p>FollowingListItem</p>
+      <p>{following.name}</p>
+      <li>
+        <Link href={`/users/${following.id}`}>
+          <a>
+            <img
+              src={user.avatarUrl || '/no_image_square.jpg'}
+              alt='user avatar'
+              className='mt-2 h-16 w-16 rounded-full'
+            />
+          </a>
+        </Link>
+      </li>
+    </div>
+  );
 };
 
 /*

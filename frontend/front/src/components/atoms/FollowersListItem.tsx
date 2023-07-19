4dc710dac28b2d38303348c4ab5106ed00432aa3
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-19 17:04:25 +0900

    add:FollowersList作成

--- frontend/front/src/components/atoms/FollowersListItem.tsx
+++ frontend/front/src/components/atoms/FollowersListItem.tsx
@@ -1,20 +1,20 @@
 import Link from 'next/link';
-import { FollowUser } from '../../types/relationship';
+import { Follower } from '../../types/relationship';
 import { User } from '../../types/user';
 
 type FollowersListItemProps = {
   // 非同期データ取得の前には、結果がまだ取得されていない（すなわちundefined）可能性がある
-  followUser: FollowUser;
+  follower: Follower;
   user: User;
 };
 
 // followersはフォローしているユーザーの配列
-export const FollowersListItem = ({ followUser, user }: FollowersListItemProps) => {
-  console.log(`そ:${JSON.stringify(followUser)}`);
+export const FollowersListItem = ({ follower, user }: FollowersListItemProps) => {
+  console.log(`そ:${JSON.stringify(follower)}`);
   return (
     <div>
       <li className='my-px flex flex-row justify-center divide-y divide-slate-200 '>
-        <Link href={`/users/${followUser.id}`}>
+        <Link href={`/users/${follower.id}`}>
           <a>
             <img
               src={user.avatarUrl || '/no_image_square.jpg'}
@@ -24,9 +24,9 @@ export const FollowersListItem = ({ followUser, user }: FollowersListItemProps) 
           </a>
         </Link>
         <div className='flex  items-center justify-center'>
-          <Link href={`/users/${followUser.id}`}>
+          <Link href={`/users/${follower.id}`}>
             <a className='ml-4  text-sm text-black text-opacity-50 hover:cursor-pointer hover:text-basic-pink md:text-xl'>
-              {followUser.name}
+              {follower.name}
             </a>
           </Link>
         </div>

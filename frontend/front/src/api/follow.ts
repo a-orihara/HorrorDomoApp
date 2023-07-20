d7ee8f07ecc8bee96152cd812c6a9e0c5d66ea57
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-20 21:08:40 +0900

    fix:FollowFormでフォローによってボタンを切り替える処理を修正

--- frontend/front/src/api/follow.ts
+++ frontend/front/src/api/follow.ts
@@ -40,7 +40,7 @@ export const createFollow = (userId: number) => {
   });
 };
 
-// ユーザーが特定のユーザーをフォローしているか確認するAPI
+// 1 ユーザーが特定のユーザーをフォローしているか確認するAPI
 export const isFollowing = (userId: number, otherUserId: number) => {
   return client.post(
     `/users/${userId}/is_following`,
@@ -83,3 +83,19 @@ export const getFollowersByUserId = async (page: number, itemsPerPage: number, u
     },
   });
 };
+
+/*
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+APIリクエストのbodyに含めるとは、HTTPリクエストを送信する際に、ヘッダー以外の部分に情報を添付することを指します。
+ここでの情報は主にサーバー側で処理するためのデータで、JSON形式などで送られます。Axiosを使ってリクエストを送信する
+際には、通常`axios.post(url, data, config)`という形式で関数を呼び出します。ここで`data`がリクエストのbodyに
+該当します。
+つまり、今回の`other_id: otherUserId`はAPIリクエストのbody部分に含まれるデータで、サーバー側で
+`params[:other_id]`として取り出されます。
+------------------------------------------------------------------------------------------------
+Axiosのpostメソッドの第三引数のconfig部分には、リクエストに関する設定をオブジェクト形式で渡します。
+このconfigオブジェクトのheadersプロパティには、HTTPリクエストヘッダに追加したい情報を指定します。
+headerの部分は、認証情報をHTTPヘッダとして送信するための設定です。これによりサーバ側で認証を行い、リクエストを正し
+く処理することができます。
+*/
--- frontend/front/src/components/molecules/FollowForm.tsx
+++ frontend/front/src/components/molecules/FollowForm.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from 'react';
+import { isFollowing } from '../../api/follow';
 import Button from '../atoms/Button';
 
-export const FollowForm = () => {
-  const test = () => {
-    console.log('FollowForm');
-  };
+type FollowFormProps = {
+  userId: number;
+  otherUserId: number;
+};
+
+export const FollowForm = ({ userId, otherUserId }: FollowFormProps) => {
+  const [isFollowed, setIsFollowed] = useState<boolean>(false);
+
+  useEffect(() => {
+    const checkFollow = async () => {
+      const response = await isFollowing(userId, otherUserId);
+      setIsFollowed(response.data.is_following);
+    };
+    checkFollow();
+  }, [userId, otherUserId]);
+
+  // const test = () => {
+  //   console.log('FollowForm');
+  // };
 
   return (
     <div>
-      <form className='bg-red-200' action='post' onSubmit={test}>
-        <Button className='m-auto mt-3 rounded-lg bg-basic-yellow font-semibold hover:bg-hover-yellow'>follow</Button>
-        <Button className='m-auto mt-3 rounded-lg bg-basic-yellow font-semibold hover:bg-hover-yellow'>unfollow</Button>
+      <form className='bg-red-200' action='post'>
+        {isFollowed ? (
+          <Button className='m-auto mt-3 rounded-lg bg-basic-yellow font-semibold hover:bg-hover-yellow'>
+            unfollow
+          </Button>
+        ) : (
+          <Button className='m-auto mt-3 rounded-lg bg-basic-yellow font-semibold hover:bg-hover-yellow'>follow</Button>
+        )}
       </form>
     </div>
   );

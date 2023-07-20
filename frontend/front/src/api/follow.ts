f45a268bffec7a98694abfcec3c7f887e9b10e04
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-20 16:11:04 +0900

    add:createFollowを作成

--- frontend/front/src/api/follow.ts
+++ frontend/front/src/api/follow.ts
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 import client from './client';
 
-// ユーザーのフォロー情報を取得するAPI
+// ユーザーのフォローユーザーの総数を取得するAPI
 export const getFollowingCountByUserId = (userId: number | undefined) => {
   // export const getUserFollowingByUserId = (userId: number) => {
   return client.get(`/users/${userId}/following`, {
@@ -13,6 +13,7 @@ export const getFollowingCountByUserId = (userId: number | undefined) => {
   });
 };
 
+// ユーザーのフォロー情報を取得するAPI
 export const getFollowingByUserId = async (page: number, itemsPerPage: number, userId: number | undefined) => {
   console.log(`getFollowingByUserIdの${userId}`);
   return client.get(`/users/${userId}/following`, {
@@ -28,7 +29,18 @@ export const getFollowingByUserId = async (page: number, itemsPerPage: number, u
   });
 };
 
-// ユーザーのフォロワー情報を取得するAPI
+// ユーザーをフォローするAPI
+export const createFollow = (userId: number) => {
+  return client.post(`/users/${userId}/following`, {
+    headers: {
+      'access-token': Cookies.get('_access_token'),
+      client: Cookies.get('_client'),
+      uid: Cookies.get('_uid'),
+    },
+  });
+};
+
+// ユーザーのフォロワーの総数を取得するAPI
 export const getFollowersCountByUserId = (userId: number | undefined) => {
   return client.get(`/users/${userId}/followers`, {
     headers: {
@@ -39,6 +51,7 @@ export const getFollowersCountByUserId = (userId: number | undefined) => {
   });
 };
 
+// ユーザーのフォロワー情報を取得するAPI
 export const getFollowersByUserId = async (page: number, itemsPerPage: number, userId: number | undefined) => {
   // console.log(`getFollowingByUserIdの${userId}`);
   return client.get(`/users/${userId}/followers`, {

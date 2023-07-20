76cfa3c890ac7141971568b5fd3d7c9c7399bec2
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-20 16:04:37 +0900

    add:create,destroyを仮実装

--- backend/api/app/controllers/api/v1/relationships_controller.rb
+++ backend/api/app/controllers/api/v1/relationships_controller.rb
@@ -1,2 +1,20 @@
 class Api::V1::RelationshipsController < ApplicationController
+  # 1
+  before_action :authenticate_api_v1_user!
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
+
+=begin
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+authenticate_api_v1_user!
+Devise Token Authによって提供されるメソッドで、APIリクエストが認証済みのユーザーから来ているかどうかを確認しま
+す。認証されていない場合は、401エラーを返します。
+=end

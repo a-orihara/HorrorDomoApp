f81ffccb9ed550414e0c69bd8357ee058b235a9a
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-25 19:03:34 +0900

    fix:不要になったFollower型を削除

--- frontend/front/src/types/follow.ts
+++ frontend/front/src/types/follow.ts
@@ -11,17 +11,3 @@ export type FollowUser = {
   profile: string | null;
   avatarUrl: string | null;
 };
-
-export type Follower = {
-  id: number;
-  uid: string;
-  provider: string;
-  email: string;
-  name: string;
-  allowPasswordChange: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-  admin: boolean;
-  profile: string | null;
-  avatarUrl: string | null;
-};

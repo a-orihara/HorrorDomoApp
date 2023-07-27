819d2be7ee0f17c701ac9371dce92359a79b3ce1
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-27 17:55:22 +0900

    add:likesのmigrationにindexを追加

--- backend/api/db/migrate/20230727062158_create_likes.rb
+++ backend/api/db/migrate/20230727062158_create_likes.rb
@@ -5,6 +5,31 @@ class CreateLikes < ActiveRecord::Migration[6.1]
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
+
+      # 1
+      t.index :user_id
+      t.index :micropost_id
+      # 2
+      t.index [:user_id, :micropost_id], unique: true
     end
   end
 end
+
+=begin
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+`add_index`と`t.index`は、どちらもRailsのマイグレーションにおけるメソッドで、データベースのテーブルにインデック
+スを追加するために使われます。
+違いは以下の通りです：
+- `add_index`は`change`または`up`メソッドの中で使われます。これは既存のテーブルに対してインデックスを追加したり
+、削除したりする場合に使用します。
+- `t.index`は`create_table`ブロックの中で使われます。新しいテーブルを作成する際に、その作成プロセスの中で直接イ
+ンデックスを追加する場合に使用します。
+
+両者の挙動は基本的に同じです。どちらもデータベースにインデックスを追加し、データの読み出し速度を向上させるために使用
+します。適用するタイミングや文脈が違うだけです。
+
+================================================================================================
+2
+[db/migrate/20230711103017_create_relationships.rb]の2の説明を参照
+=end

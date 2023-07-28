e364f5448e6a87b93ab62fbd4b02c1630e6615e5
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-07-28 15:21:14 +0900

    add:アイコンの色を変更できる機能を追加

--- frontend/front/src/components/atoms/LikeButtonIcon.tsx
+++ frontend/front/src/components/atoms/LikeButtonIcon.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+
+// 1
 export const LikeButtonIcon = () => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  // アイコンがクリックされたときのハンドラ。いいね状態を反転する
+  const handleClick = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
-    <div>
+    <button onClick={handleClick}>
       <svg
         xmlns='http://www.w3.org/2000/svg'
-        fill='none'
+        fill={isLiked ? '#F22C5A' : 'none'}
         viewBox='0 0 24 24'
         strokeWidth={1.5}
         stroke='currentColor'
@@ -15,6 +25,15 @@ export const LikeButtonIcon = () => {
           d='M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z'
         />
       </svg>
-    </div>
+    </button>
   );
 };
+
+/*
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+SVGアイコンの色を変えるためには、アイコンがクリックされたことを検知し、その状態に応じてSVGの`fill`属性の値を変える
+必要があります。このためには、通常、Reactの`useState`フックと`onClick`イベントハンドラが使われます。また、SVGア
+イコンの`fill`属性を`{isLiked ? "pink" : "none"}`のように設定します。`isLiked`が真（つまりアイコンが「いいね
+」されている状態）の場合、`fill`属性は`"pink"`になります。
+*/

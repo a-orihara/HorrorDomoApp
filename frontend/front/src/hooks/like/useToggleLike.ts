9c6157324e56396908e2b7ea0ca9a1cbf661e10d
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-02 17:23:59 +0900

    fix:不要なhandleGetAllLikesByOtherUserIdの処理を削除

--- frontend/front/src/hooks/like/useToggleLike.ts
+++ frontend/front/src/hooks/like/useToggleLike.ts
@@ -4,15 +4,15 @@ import { useAlertContext } from '../../contexts/AlertContext';
 import { useAuthContext } from '../../contexts/AuthContext';
 import { useLikeContext } from '../../contexts/LikeContext';
 
-// いいねのトグルフック。いいね総数を更新する為、引数にuserIdを追加
+// いいねのトグルフック。いいね総数を更新する為、引数にuserId（/users/:idのid）を追加
 export const useToggleLike = (liked: boolean, postId: number, userId: number) => {
   // いいね済みかの真偽値。初期値はBDから取得したpostのliked
   const [isLiked, setIsLiked] = useState<boolean>(liked);
   const { currentUser } = useAuthContext();
   // いいねがトグルされたらいいね総数を更新する為、handleGetAllLikesを取得
-  const { handleGetAllLikesByCurrentUserId, handleGetAllLikesByOtherUserId } = useLikeContext();
+  const { handleGetAllLikesByCurrentUserId } = useLikeContext();
   const { setAlertMessage, setAlertOpen, setAlertSeverity } = useAlertContext();
-  console.log(`post.idは:${postId}、likeの状態は:${isLiked}`);
+  console.log(`user.idは:${userId}、likeの状態は:${isLiked}`);
 
   const handleToggleLike = async () => {
     try {
@@ -25,16 +25,17 @@ export const useToggleLike = (liked: boolean, postId: number, userId: number) =>
         res = await createLike(postId);
         setIsLiked(true);
       }
+      // トグルが成功したら、currentUserのいいね総数を更新する
       if (res.status === 200 || res.status === 201) {
-        // isLikedをfalseからtrue、またはtrueからfalseに変更
-        setIsLiked(!isLiked);
-        if (currentUser && userId === currentUser.id) {
+        // このuserIdはusers/:idのid
+        if (currentUser) {
           handleGetAllLikesByCurrentUserId(currentUser.id);
-        } else {
-          // いいね総数を更新
-          console.log('handleGetAllLikesByOtherUserIdが発火');
-          handleGetAllLikesByOtherUserId(userId);
         }
+        // } else {
+        //   // いいね総数を更新
+        //   console.log('handleGetAllLikesByOtherUserIdが呼ばれた');
+        //   handleGetAllLikesByOtherUserId(userId);
+        // }
       }
     } catch (err: any) {
       setAlertSeverity('error');

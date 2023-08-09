85b69aa7dc1d8a8a128e4ad097e217e06f84c629
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-09 18:20:49 +0900

    fix:型のプロパティ名を修正（テストコード）

--- frontend/front/src/components/molecules/TestMovie.tsx
+++ frontend/front/src/components/molecules/TestMovie.tsx
@@ -4,15 +4,15 @@ import { tmdbClient } from '../../api/client';
 type Res = {
   title: string;
   overview: string;
-  poster_path: string;
+  posterPath: string;
 };
 
 export const TestMovie = () => {
   const [movieInfo, setMovieInfo] = useState<Res | null>(null);
 
   const handleButtonClick = async () => {
-    const movieTitle = '83564308256f1f54d8e8ee725927afff';
-    const apiKey = '任意';
+    const movieTitle = '用心棒';
+    const apiKey = '83564308256f1f54d8e8ee725927afff';
     // const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTitle}&language=ja&region=JP`;
     // const response = await axios.get(url);
     const url = `/search/movie?api_key=${apiKey}&query=${movieTitle}&language=ja&region=JP`; // baseURLを使用
@@ -33,7 +33,7 @@ export const TestMovie = () => {
         <div>
           <p>{movieInfo.title}</p>
           <p>{movieInfo.overview}</p>
-          <img src={`https://image.tmdb.org/t/p/w500${movieInfo.poster_path}`} alt={movieInfo.title} />
+          <img src={`https://image.tmdb.org/t/p/w500${movieInfo.posterPath}`} alt={movieInfo.title} />
         </div>
       ) : (
         <p>まだない</p>

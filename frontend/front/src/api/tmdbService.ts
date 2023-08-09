e248b48649ddb8d95a2be2d23e0ba3d7c4fa663f
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-09 18:31:10 +0900

    add:api/tmdbService.tsを仮作成

---
+++ frontend/front/src/api/tmdbService.ts
@@ -0,0 +1,10 @@
+// 2 ユーザー情報を更新
+// export const getTmbdInfo = () => {
+//   return tmdbClient.put('/auth' {
+//     headers: {
+//       'access-token': Cookies.get('_access_token'),
+//       client: Cookies.get('_client'),
+//       uid: Cookies.get('_uid'),
+//     },
+//   });
+// };

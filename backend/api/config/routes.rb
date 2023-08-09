9702a7fe1ea9250f5eb8c2540a1bbb3676ee9e4f
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-09 18:45:33 +0900

    add:movies_controllerを追加

--- backend/api/config/routes.rb
+++ backend/api/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'movies/index'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       # 1 api/v1/auth
       mount_devise_token_auth_for 'User', at: 'auth', controllers: {
         registrations: 'api/v1/auth/registrations',

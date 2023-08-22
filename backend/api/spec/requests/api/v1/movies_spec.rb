9cad9282ed7fa4d51f09d6001819f855797f34a2
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-22 19:50:48 +0900

    add:requests/api/v1/movies_spec.rbを作成

--- backend/api/spec/requests/api/v1/movies_spec.rb
+++ backend/api/spec/requests/api/v1/movies_spec.rb
@@ -1,10 +1,36 @@
+# backend/api/spec/requests/api/v1/movies_spec.rb
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Movies", type: :request do
-  # describe "GET /index" do
-  #   it "returns http success" do
-  #     get "/api/v1/movies/index"
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  let(:user) { create(:user) }
+  let(:headers) { request_login_user(user) }
+  let(:title) { "ジョーズ" }
+
+  describe "GET /index" do
+    context 'ユーザーが認証されている場合' do
+      before do
+        get api_v1_movies_path, params: { title: title }, headers: headers
+      end
+
+      it 'ステータスコード200を返すこと' do
+        expect(response).to have_http_status(200)
+      end
+
+      it '映画情報を返すこと' do
+        json = JSON.parse(response.body)
+        puts "ここjson:#{json['data']}"
+        expect(json['data']).to be_present
+      end
+    end
+
+    context 'ユーザーが認証されていない場合' do
+      before do
+        get api_v1_movies_path, params: { title: title }
+      end
+
+      it 'ステータスコード401を返すこと' do
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
 end

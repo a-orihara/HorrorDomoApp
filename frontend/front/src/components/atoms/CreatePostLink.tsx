af7561a5392392275f355955c8f850a6befe4ffa
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-22 11:04:16 +0900

    fix:UserInfoのレイアウト修正

--- frontend/front/src/components/atoms/CreatePostLink.tsx
+++ frontend/front/src/components/atoms/CreatePostLink.tsx
@@ -5,7 +5,7 @@ const CreatePostLink = () => {
   return (
     <section>
       <Link href={'/post/new'}>
-        <a className='mb-2 flex items-center justify-center rounded-full border-2 bg-basic-yellow  p-1 text-xl font-semibold hover:cursor-pointer hover:text-basic-pink lg:text-2xl'>
+        <a className='mb-2 flex items-center justify-center rounded-full border-2 border-black  bg-basic-green p-1 text-xl font-semibold text-white hover:cursor-pointer hover:bg-basic-pink lg:text-2xl'>
           Let&apos;s post!
         </a>
       </Link>

5e8835e54eeef7e0df8534d4c4556d3fdd5632f6
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-22 14:04:19 +0900

    add:models/like_spec.rbを作成

--- backend/api/spec/models/like_spec.rb
+++ backend/api/spec/models/like_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  # 必要なファクトリを使用してlikeオブジェクトを作成します
+  let(:like) { build(:like) }
+
+  describe 'アソシエーション' do
+    it 'postに属する' do
+      expect(like).to belong_to(:post)
+    end
+
+    it 'userに属する' do
+      expect(like).to belong_to(:user)
+    end
+  end
+
+  describe 'バリデーション' do
+    it 'user_idが存在する' do
+      expect(like).to validate_presence_of(:user_id)
+    end
+
+    it 'post_idが存在する' do
+      expect(like).to validate_presence_of(:post_id)
+    end
+  end
 end
--- backend/api/spec/requests/api/v1/likes_spec.rb
+++ backend/api/spec/requests/api/v1/likes_spec.rb
@@ -1,7 +1,44 @@
+# backend/api/spec/requests/api/v1/likes_spec.rb
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Likes", type: :request do
-  # describe "GET /index" do
-  #   pending "add some examples (or delete) #{__FILE__}"
-  # end
+RSpec.describe 'Api::V1::Likes', type: :request do
+  describe 'いいね機能' do
+    let(:user) { create(:user) }
+    let(:post) { create(:post) }
+
+    before do
+      sign_in user # TestMacrosによるサインイン
+    end
+
+    describe 'いいね作成' do
+      context '有効なパラメータの場合' do
+        it 'いいねが作成される' do
+          expect do
+            post api_v1_post_likes_path(post_id: post.id)
+          end.to change(Like, :count).by(1)
+          expect(response).to have_http_status(:ok)
+        end
+      end
+
+      context '無効なパラメータの場合' do
+        it 'いいねが作成されない' do
+          expect do
+            post api_v1_post_likes_path(post_id: nil)
+          end.to change(Like, :count).by(0)
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+      end
+    end
+
+    describe 'いいね削除' do
+      let!(:like) { create(:like, user: user, post: post) }
+
+      it 'いいねが削除される' do
+        expect do
+          delete destroy_api_v1_post_likes_path(post_id: post.id)
+        end.to change(Like, :count).by(-1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
 end

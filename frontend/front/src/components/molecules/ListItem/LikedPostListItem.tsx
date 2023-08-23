2a815f5287378336b908f071fa7636ac4180301b
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-08-23 20:54:57 +0900

    add:LikedPostListItemにUserAndPostLinksを追加

--- frontend/front/src/components/molecules/ListItem/LikedPostListItem.tsx
+++ frontend/front/src/components/molecules/ListItem/LikedPostListItem.tsx
@@ -1,10 +1,10 @@
 // date-fns-tzパッケージを使ってUTC時間を日本時間に変換し、フォーマットを指定
-import Link from 'next/link';
 import { useAuthContext } from '../../../contexts/AuthContext';
 import { useDeletePost } from '../../../hooks/post/useDeletePost';
 import { Post } from '../../../types/post';
 import { User } from '../../../types/user';
 import UserAvatar from '../../atoms/UserAvatar';
+import UserAndPostLinks from '../frontend/front/src/components/molecules/UserAndPostLinks';
 import ListItemContent from './ListItemContent';
 
 // LikedPostListItemPropsはkey名がpostで値にLikedPost型を持つオブジェクト型;
@@ -25,17 +25,7 @@ const LikedPostListItem = ({ likedPost, likedUser }: LikedPostListItemProps) => 
       <div className='flex'>
         <UserAvatar avatarUrl={likedUser.avatarUrl} userId={likedUser.id}></UserAvatar>
         <div>
-          <p>
-            <Link href={`/users/${likedUser.id}`}>
-              <a className='text-xs lg:text-base lg:tracking-wider'>{likedUser.name}</a>
-            </Link>
-          </p>
-          {/* <p className='text-sm md:text-xl'>タイトル:{likedPost.title}</p> */}
-          <Link href={`/post/${likedPost.id}`}>
-            <a className='text-sm text-black  text-opacity-50 hover:cursor-pointer hover:text-basic-pink md:text-xl'>
-              {likedPost.title}
-            </a>
-          </Link>
+          <UserAndPostLinks user={likedUser} post={likedPost}></UserAndPostLinks>
           <ListItemContent
             post={likedPost}
             user={likedUser}

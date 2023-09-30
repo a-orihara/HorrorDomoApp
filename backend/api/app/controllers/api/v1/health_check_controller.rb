b1f4a3f916bb7835a6b5fb085e92e4b9d795b87a
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-09-30 14:49:55 +0900

    fix:rubocop修正

--- backend/api/app/controllers/api/v1/health_check_controller.rb
+++ backend/api/app/controllers/api/v1/health_check_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::HealthCheckController < ApplicationController
   def index
-    render json: { message: "成功：Success Health Check!" }, status: :ok
+    render json: { message: "成功:Success Health Check!" }, status: :ok
   end
 end
 
--- backend/api/spec/requests/api/v1/health_check_spec.rb
+++ backend/api/spec/requests/api/v1/health_check_spec.rb
@@ -1,7 +1,14 @@
-require 'rails_helper'
+# backend/api/spec/requests/api/v1/health_check_spec.rb
+require "rails_helper"
 
-RSpec.describe "Api::V1::HealthChecks", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Api::V1::HealthCheck", type: :request do
+  describe "GET api/v1/health_check" do
+    it "正常にレスポンスが返る" do
+      get(api_v1_health_check_path)
+      # res = JSON.parse(response.body) *rubocopの警告が出る
+      res = response.parsed_body
+      expect(res["message"]).to eq "成功:Success Health Check!"
+      expect(response).to have_http_status(:success)
+    end
   end
 end

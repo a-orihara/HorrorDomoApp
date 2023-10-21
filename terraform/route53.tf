01cd16adfbd5b0058ea98b0638e6ad3df440914d
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-21 12:07:06 +0900

    add:変数:domainを定義

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -5,7 +5,8 @@
 resource "aws_route53_zone" "portfolio_route53_zone_tf" {
   comment = "HostedZone created by Route53 Registrar"
   # ドメイン名
-  name    = "horror-domo-app.com"
+  # 1 var:Terraformで変数を参照するためのprefix。設定ファイル内で定義された変数を参照する為に使用
+  name     = var.domain
   tags     = {}
   tags_all = {}
 }

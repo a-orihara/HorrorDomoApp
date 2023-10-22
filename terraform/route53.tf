3fe2827700c2adca265b7d621039313b90c582e4
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-22 14:28:28 +0900

    add:pubとprivのaws_security_groupを作成
    aws_security_group_ruleも合わせて作成

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,5 +1,5 @@
 # ---------------------------------------------
-# Route53
+# Route53 "aws_route53_zone"
 # ---------------------------------------------
 
 resource "aws_route53_zone" "portfolio_route53_zone_tf" {
@@ -11,3 +11,31 @@ resource "aws_route53_zone" "portfolio_route53_zone_tf" {
   tags_all = {}
 }
 
+# ---------------------------------------------
+# Route53 "aws_route53_record"
+# ---------------------------------------------
+# 1
+resource "aws_route53_record" "portfolio_route53_record_a" {
+  # fqdn    = "horror-domo-app.com" /いらないっぽい
+  # id      = "Z05743042OK49Y65Q6CSP_horror-domo-app.com_A"/いらないっぽい
+  name    = "horror-domo-app.com"
+  records = []
+  ttl     = 0
+  type    = "A"
+  zone_id = aws_route53_zone.portfolio_route53_zone_tf.id
+  # alias {
+  #     evaluate_target_health = true
+  #     # albのidが必要
+  #     name                   = "portfolio-frontend-alb-1834571258.ap-northeast-1.elb.amazonaws.com"
+  #     # albのzon_idが必要?
+  #     zone_id                = "Z14GRHDCWA56QT"
+  # }
+}
+
+
+/*
+@          @@          @@          @@          @@          @@          @@          @@          @
+1
+Route53のレコードをインポートするためには、ホストゾーンID・レコード名・レコードタイプをアンダースコアで区切ったID
+を指定する必要があります。
+*/
--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,78 +1,122 @@
-# ---------------------------------------------
-# 1 Security Group
-# ---------------------------------------------
+# ================================================================================================
+# Security Group "pub"
+# ================================================================================================
+# 1 ここでは、セキュリティグループのルールを除外する(no ingress or egress rules)
+resource "aws_security_group" "portfolio_pub_sg_tf" {
+  # 1.1
+  description = "portfolio-pub-sg"
+  name        = "portfolio-pub-sg"
+  vpc_id      = aws_vpc.portfolio_vpc_tf.id
+}
 
-# ここでは、セキュリティグループのルールを除外する(no ingress or egress rules)
+# ------------------------------------------------------------------------------------------------
+# aws_security_group_rule out
+# ------------------------------------------------------------------------------------------------
+# 1.2
+resource "aws_security_group_rule" "portfolio_pub_sg_in_http_ipv4_tf" {
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 80
+  ipv6_cidr_blocks  = []
+  protocol = "tcp"
+  security_group_id = aws_security_group.portfolio_pub_sg_tf.id
+  to_port = 80
+  type    = "ingress"
+}
 
+resource "aws_security_group_rule" "portfolio_pub_sg_in_http_ipv6_tf" {
+  cidr_blocks       = []
+  # 送信元ポートの設定
+  from_port = 80
+  ipv6_cidr_blocks = [
+    # "::/0"はIPv6の全てのアドレスを表すCIDRブロックで、すべてのIPv6トラフィックを許可
+    "::/0",
+  ]
+  # TCPプロトコルのトラフィックを許可
+  protocol = "tcp"
+  # このプロパティはIPv6のCIDRブロックを指定
+  security_group_id = aws_security_group.portfolio_pub_sg_tf.id
+  to_port = 80
+  # インバウンドルール
+  type = "ingress"
+}
 
-# resource "aws_security_group" "portfolio_pub_sg_tf" {
-#   # 1.1
-#   name        = "portfolio-pub-sg"
-#   description = "portfolio-pub-sg"
-#   vpc_id      = aws_vpc.portfolio-vpc-tf.id
-#   # タグなど、必要な他の属性もここで定義可能
+# ------------------------------------------------------------------------------------------------
+# aws_security_group_rule out
+# ------------------------------------------------------------------------------------------------
+# 1.3 アウトバウンドはデフォルトの設定
+resource "aws_security_group_rule" "portfolio_pub_sg_out_all_tf" {
+  cidr_blocks = ["0.0.0.0/0"]
+  # 送信元ポートの設定です。この値が0の場合、すべてのポートを許可することを意味します。
+  from_port = 0
+  # トラフィックのプロトコルを指定。"-1"はすべてのプロトコルを許可（制限しない）。
+  ipv6_cidr_blocks  = []
+  protocol = "-1"
+  security_group_id = aws_security_group.portfolio_pub_sg_tf.id
+  # 送信先ポートの設定です。同様に、この値も0の場合、すべてのポートを許可することを示しています。
+  to_port = 0
+  # アウトバウンドルール
+  type = "egress"
+}
+resource "aws_security_group_rule" "portfolio_pub_sg_out_rds_tf" {
+  # 競合する為、cidr_blocksとsource_security_group_idと同時に設定しない
+  # cidr_blocks              = []
+  from_port                = 3306
+  # 競合する為、cidr_blocksとsource_security_group_idと同時に設定しない
+  # ipv6_cidr_blocks         = []
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.portfolio_pub_sg_tf.id
+  source_security_group_id = aws_security_group.portfolio_priv_sg_tf.id
+  to_port                  = 3306
+  type                     = "egress"
+}
 
-#   tags = {
-#     Name = "portfolio-pub-sg"
-#   }
-# }
-
+# ================================================================================================
+# Security Group "priv"
+# ================================================================================================
 
 # portfolio-priv-sg
-# resource "aws_security_group" "portfolio-priv-sg-tf" {
-#   name        = "portfolio-priv-sg"
-#   description = "portfolio-priv-sg"
-#   vpc_id      = aws_vpc.portfolio-vpc-tf.id
-#   depends_on = [aws_security_group.portfolio_pub_sg_tf]
+resource "aws_security_group" "portfolio_priv_sg_tf" {
+  description = "portfolio-priv-sg"
+  name        = "portfolio-priv-sg"
+  vpc_id      = aws_vpc.portfolio_vpc_tf.id
+}
 
-#   ingress {
-#     from_port       = 3306
-#     to_port         = 3306
-#     protocol        = "tcp"
-#     security_groups = [aws_security_group.portfolio-pub-sg-tf.id]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   // Optional: Add tags for additional metadata
-#   tags = {
-#     Name = "portfolio-priv-sg"
-#   }
-
-# }
-
-
-# resource "aws_security_group_rule" "portfolio_pub_sg_in_http_ipv4_tf" {
-#     type              = "ingress"
-#     from_port         = 80
-#     to_port           = 80
-#     protocol          = "tcp"
-#     cidr_blocks       = ["0.0.0.0/0"]
-#     security_group_id = aws_security_group.portfolio_pub_sg_tf.id
-# }
+# ------------------------------------------------------------------------------------------------
+# aws_security_group_rule out
+# ------------------------------------------------------------------------------------------------
+resource "aws_security_group_rule" "portfolio_priv_sg_in_rds_tf" {
+    from_port                = 3306
+    protocol                 = "tcp"
+    security_group_id        = aws_security_group.portfolio_priv_sg_tf.id
+    source_security_group_id = aws_security_group.portfolio_pub_sg_tf.id
+    to_port                  = 3306
+    type                     = "ingress"
+}
+resource "aws_security_group_rule" "portfolio_priv_sg_out_all_tf" {
+    cidr_blocks       = [
+        "0.0.0.0/0",
+    ]
+    from_port         = 0
+    ipv6_cidr_blocks  = []
+    protocol          = "-1"
+    security_group_id = aws_security_group.portfolio_priv_sg_tf.id
+    to_port           = 0
+    type              = "egress"
+}
 
 
-# resource "aws_security_group_rule" "portfolio-pub-sg-in-http-ipv6-tf" {
-#   security_group_id = aws_security_group.portfolio-pub-sg-tf.id
-#   type              = "ingress"
-#   protocol          = "tcp"
-#   from_port         = 80
-#   to_port           = 80
-#   ipv6_cidr_blocks  = [
-#         "::/0",
-#     ]
-# }
+
+
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @
 1
+aws_security_groupとaws_security_group_ruleの両方でルールを設定してはいけない。
 sgの作成には、"aws_security_group"と、"aws_security_group_rule"の二つのresourceが基本的に必要。
 ただし、既にawsのリソースを作成済みの場合、"aws_security_group"に続いて、"aws_security_group_rule"を、
 terraform importすると、"aws_security_group"をimportした時点でインバウンド、アウトバウンドがimportされるの
 で、さらに"aws_security_group_rule"をimportすると、インバウンド、アウトバウンドのルールが競合する。
-その為、"aws_security_group"の作成のみで良い。
+その為、"aws_security_group"の作成の際は、"aws_security_group"内にインバウンド、アウトバウンドのルールの設
+定は書き込まない。その次に"aws_security_group_rule"を設定すると上手くいく。
 
 ================================================================================================
 1.1
@@ -100,4 +144,17 @@ terraform state rm 'aws_security_group.portfolio_pub_sg_tf.ingress'
 Note: You didn't use the -out option to save this plan, so Terraform can't
 guarantee to take exactly these actions if you run "terraform apply" now.
 
+================================================================================================
+1.2
+- `aws_security_group_rule`リソースを`terraform import`する際の一般的なコマンド書式:
+terraform import aws_security_group_rule.<resource_name> <sg_id>_<type>_<protocol>_<from_port>_<to_port>_<source/destination>
+- 実際の例
+terraform import aws_security_group_rule.portfolio_pub_sg_in_http_ipv4_tf sg-0264949bc0da0c58e_ingress_tcp_80_80_0.0.0.0/0
+
+================================================================================================
+1.3
+アウトバウンドルール（デフォルトの設定）
+タイプ：すべてのトラフィック、プロトコル：すべて、ポート範囲：すべて、送信先 ：カスタム 0.0.0.0/0は、
+terraform import aws_security_group_rule.portfolio_pub_sg_out_all_tf sg-0264949bc0da0c58e_egress_all_0_65535_0.0.0.0/0
+で取り込み
 */

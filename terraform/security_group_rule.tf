10f1f3def1bae45568f42bb83077f3932b9c0565
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-23 10:34:58 +0900

    add:"aws_security_group_rule" "portfolio_front"
    inとout

--- terraform/security_group_rule.tf
+++ terraform/security_group_rule.tf
@@ -100,6 +100,34 @@ resource "aws_security_group_rule" "portfolio_front_sg_in_http_ipv4_tf" {
   to_port           = 80
   type              = "ingress"
 }
+
+resource "aws_security_group_rule" "portfolio_front_sg_in_http_ipv6_tf" {
+    cidr_blocks       = []
+    from_port         = 80
+    ipv6_cidr_blocks  = [
+        "::/0",
+    ]
+    protocol          = "tcp"
+    security_group_id = aws_security_group.portfolio_front_sg_tf.id
+    to_port           = 80
+    type              = "ingress"
+}
+
+# ================================================================================================
+# portfolio_front out
+# ================================================================================================
+resource "aws_security_group_rule" "portfolio_front_sg_out_all_tf" {
+    cidr_blocks       = [
+        "0.0.0.0/0",
+    ]
+    from_port         = 0
+    ipv6_cidr_blocks  = []
+    protocol          = "-1"
+    security_group_id = aws_security_group.portfolio_front_sg_tf.id
+    to_port           = 0
+    type              = "egress"
+}
+
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @
 ================================================================================================
@@ -107,8 +135,10 @@ resource "aws_security_group_rule" "portfolio_front_sg_in_http_ipv4_tf" {
 - `aws_security_group_rule`リソースを`terraform import`する際の一般的なコマンド書式:
 terraform import aws_security_group_rule.<resource_name> <sg_id>_<type>_<protocol>_<from_port>_<to_port>_<source/destination>
 - 実際の例
+iPV4:0.0.0.0/0
 terraform import aws_security_group_rule.portfolio_pub_sg_in_http_ipv4_tf sg-0264949bc0da0c58e_ingress_tcp_80_80_0.0.0.0/0
-
+iPV6:::/0
+terraform import aws_security_group_rule.portfolio_front_sg_in_http_ipv6_tf sg-0420324d5aebff0c7_ingress_tcp_80_80_::/0
 ================================================================================================
 2
 アウトバウンドルール（デフォルトの設定）

d8d03841929d41b23dc02c39ad1c2b9e44eaf94f
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-23 13:51:41 +0900

    add:"aws_lb" "portfolio_frontend_alb_tf"

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -24,16 +24,17 @@ resource "aws_lb" "portfolio_alb_tf" {
   ]
   tags     = {}
   tags_all = {}
+  # このコンフィギュレーションを適用した結果に基づいて自動的に決定されます。
   # vpc_id   = aws_vpc.portfolio_vpc_tf.id
   # zone_id  = "Z14GRHDCWA56QT"
   # access_logs {
   #     enabled = false
   # }
   subnet_mapping {
-    subnet_id = "subnet-02014c0834197d87c"
+    subnet_id = aws_subnet.portfolio_pub_subnet_a_tf.id
   }
   subnet_mapping {
-    subnet_id = "subnet-09213ebf10410a7d3"
+    subnet_id = aws_subnet.portfolio_pub_subnet_c_tf.id
   }
   timeouts {}
 }
@@ -72,6 +73,47 @@ resource "aws_lb_listener" "portfolio_alb_listener_https" {
   }
   timeouts {}
 }
+
+# ---------------------------------------------
+# frontend_alb
+# ---------------------------------------------
+resource "aws_lb" "portfolio_frontend_alb_tf" {
+  desync_mitigation_mode           = "defensive"
+  # dns_name                         = "portfolio-frontend-alb-1834571258.ap-northeast-1.elb.amazonaws.com"
+  drop_invalid_header_fields       = false
+  enable_cross_zone_load_balancing = true
+  enable_deletion_protection       = false
+  enable_http2                     = true
+  enable_waf_fail_open             = false
+  idle_timeout                     = 60
+  internal                         = false
+  ip_address_type                  = "ipv4"
+  load_balancer_type               = "application"
+  name                             = "portfolio-frontend-alb"
+  security_groups                  = [
+      aws_security_group.portfolio_alb_frontend_sg_tf.id,
+  ]
+  subnets = [
+  aws_subnet.portfolio_pub_subnet_a_tf.id,
+  aws_subnet.portfolio_pub_subnet_c_tf.id,
+  ]
+  tags                             = {}
+  tags_all                         = {}
+  # vpc_id                           = "vpc-0fd0788c8a372ee22"
+  # zone_id                          = "Z14GRHDCWA56QT"
+  # access_logs {
+  #     enabled = false
+  # }
+  subnet_mapping {
+      subnet_id = aws_subnet.portfolio_pub_subnet_a_tf.id
+  }
+  subnet_mapping {
+      subnet_id = aws_subnet.portfolio_pub_subnet_c_tf.id
+  }
+  timeouts {}
+}
+
+
 
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @

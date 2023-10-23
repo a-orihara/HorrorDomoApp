b81abcf1e7277bb1787199ee90baa23af0191a68
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-23 11:17:02 +0900

    add:"aws_security_group_rule"
    "portfolio_alb_frontend_sg out"

--- terraform/security_group_rule.tf
+++ terraform/security_group_rule.tf
@@ -179,6 +179,18 @@ resource "aws_security_group_rule" "portfolio_alb_frontend_sg_in_https_ipv6_tf" 
   type              = "ingress"
 }
 
+# ================================================================================================
+# alb_frontend out
+# ================================================================================================
+# 3
+resource "aws_security_group_rule" "portfolio_alb_frontend_sg_out_front_tf" {
+  from_port                = 80
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.portfolio_alb_frontend_sg_tf.id
+  source_security_group_id = aws_security_group.portfolio_front_sg_tf.id
+  to_port                  = 80
+  type                     = "egress"
+}
 
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @
@@ -206,4 +218,9 @@ terraform import aws_security_group_rule.portfolio_front_sg_in_http_ipv6_tf sg-0
 terraform import aws_security_group_rule.portfolio_pub_sg_out_all_tf sg-0264949bc0da0c58e_egress_all_0_65535_0.0.0.0/0
 で取り込み
 
+================================================================================================
+3
+terraform import aws_security_group_rule.<resource_name> <sg_id>_<type>_<protocol>_<from_port>_<to_port>_<ソースのsgのid>
+実際の例：
+terraform import aws_security_group_rule.portfolio_alb_frontend_sg_out_front_tf sg-06e863d201ecb782a_egress_tcp_80_80_sg-0420324d5aebff0c7
 */

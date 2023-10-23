582676a1f6b2bc951d9ec9d10a33850f6b1fb29e
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-23 13:13:11 +0900

    add:"aws_lb" "portfolio_alb_tf"
    "aws_lb_target_group" "portfolio_alb_tg_tf"

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,38 +1,51 @@
 # ---------------------------------------------
-# ALB
+# alb
 # ---------------------------------------------
 # 1
-# resource "aws_lb" "portfolio_alb_tf" {
-# 読み込みの結果
-# name                             = "portfolio-alb"
-# internal                         = false
-# load_balancer_type               = "application"
-# security_groups                  = ["sg-0a8"]
-# subnets                          = ["subnet-020", "subnet-092"]
-# enable_deletion_protection       = false
-# enable_cross_zone_load_balancing = true
-# idle_timeout                     = 60
-# enable_http2                     = true
+resource "aws_lb" "portfolio_alb_tf" {
+  desync_mitigation_mode           = "defensive"
+  # dns_name                         = "portfolio-alb-741782418.ap-northeast-1.elb.amazonaws.com"
+  drop_invalid_header_fields       = false
+  enable_cross_zone_load_balancing = true
+  enable_deletion_protection       = false
+  enable_http2                     = true
+  enable_waf_fail_open             = false
+  idle_timeout                     = 60
+  internal                         = false
+  ip_address_type                  = "ipv4"
+  load_balancer_type               = "application"
+  name                             = "portfolio-alb"
+  security_groups = [
+    aws_security_group.portfolio_alb_sg_tf.id,
+  ]
+  subnets = [
+    aws_subnet.portfolio_pub_subnet_a_tf.id,
+    aws_subnet.portfolio_pub_subnet_c_tf.id,
+  ]
+  tags     = {}
+  tags_all = {}
+  # vpc_id   = aws_vpc.portfolio_vpc_tf.id
+  # zone_id  = "Z14GRHDCWA56QT"
+  # access_logs {
+  #     enabled = false
+  # }
+  subnet_mapping {
+    subnet_id = "subnet-02014c0834197d87c"
+  }
+  subnet_mapping {
+    subnet_id = "subnet-09213ebf10410a7d3"
+  }
+  timeouts {}
+}
 
-# # access_logs {
-# #     enabled = false
-# # }
+# resource "aws_lb_listener" "alb_listener_http" {
 
-# subnet_mapping {
-#     subnet_id = "subnet-020"
 # }
-
-# subnet_mapping {
-#     subnet_id = "subnet-092"
-# }
-
-# timeouts {}
-# }
-
-
 /*
 @          @@          @@          @@          @@          @@          @@          @@          @
 1
 - terraform importでaws_lbリソースをインポートするコマンドの書式:
 terraform import aws_lb.<your_resource_name> <load_balancer_arn>
+
+
 */

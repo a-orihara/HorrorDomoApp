30ec1d75763dad2712b02dd94e3194eb01095d4c
a-orihara <ko.ty.es.ty.mk@gmail.com>
2023-10-29 13:08:55 +0900

    add:"aws_acm_certificate","aws_route53_record",
    "aws_acm_certificate_validation"を作成

---
+++ terraform/output.tf
@@ -0,0 +1,7 @@
+output "route53_record_for_each_result" {
+  value = { for dvo in aws_acm_certificate.portfolio_acm_cert.domain_validation_options : dvo.domain_name => {
+    name   = dvo.resource_record_name
+    record = dvo.resource_record_value
+    type   = dvo.resource_record_type
+  } }
+}

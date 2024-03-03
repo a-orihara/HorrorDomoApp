49c02e4712347b902b729893bece82e577d757b9
a-orihara <ko.ty.es.ty.mk@gmail.com>
2024-03-03 14:49:37 +0900

    update:main.tfの変数定義をvariables.tfへ移動

--- frontend/front/src/api/movieApi.ts
+++ frontend/front/src/api/movieApi.ts
@@ -3,7 +3,7 @@ import { client } from './client';
 
 // ユーザー情報を更新
 export const getMovieInfo = (movieTitle: string) => {
-  console.log("◆getMovieInfoが発火ピピふ")
+  console.log("◆getMovieInfoが発火ピピふゴー")
   return client.get(`/movies?title=${movieTitle}`, {
     headers: {
       'access-token': Cookies.get('_access_token'),

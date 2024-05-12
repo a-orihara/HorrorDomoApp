3f41c5e350cbc3f001332a60a58027d4a90addbd
a-orihara <ko.ty.es.ty.mk@gmail.com>
2024-05-12 11:37:30 +0900

    add:コメントを修正

--- frontend/front/src/components/organisms/Header.tsx
+++ frontend/front/src/components/organisms/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className='basic-border flex h-16 flex-row items-center bg-basic-yellow font-spacemono text-2xl font-semibold outline md:text-2xl'>
       <div className='w-1/2 flex-1'>
-        {/* Horror Domo Appにはこのh1（子）の個別設定が適用される。h1にflex,items-center,justify-center設定で文字が真ん中に */}
+        {/* Horror Domo Appにはこのh1（子）の個別設定が適用。h1にflex,items-center,justify-center設定で文字が真ん中に */}
         <h1 className='text-basic-black  flex items-center justify-center text-center font-spacemono text-lg font-semibold tracking-tighter md:text-4xl'>
           Horror Domo App
         </h1>
